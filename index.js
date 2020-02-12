2c9ff0e759dbf47abca2121435ef237ebb962f12
christina kaliora <develop@pirron.one>
2020-02-12 15:04:19 +0100

    set up index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express =  requite('express');
+
+const apiRouter = require('./api/api-router.js');
+
+const server = express();
+
+server.use(express.json()); // needed to parse JSON fron the body
+
+server.use('/api', apiRouter);//for URLs beginning with /api
+
+
+server.get('/', (req, res) => {
+    res.send(`Hello, this is the Server!`);
+});
+
+const port = 5000;
+server.listen(port, ()=> {
+    console.log(`\n *** Server is running on http://localohost:${port} ***\n`)
+})
