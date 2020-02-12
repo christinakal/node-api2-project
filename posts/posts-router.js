e2e1dd6cd3cec030ea6ff52d9aba858e3fd99f60
christina kaliora <develop@pirron.one>
2020-02-12 16:22:28 +0100

    PUT req DONE

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -102,5 +102,25 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+// --- PUT REQUEST ---
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post could not be found" });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating this post"
+      });
+    });
+});
+
 // mind the S in exportS
 module.exports = router; // same as below
